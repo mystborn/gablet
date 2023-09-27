726d75b9414e531378618e87df878f97c4d8967c
mystborn <thefirewithin1017@gmail.com>
2023-09-26 23:26:22 -0500

    docker-compose testing

--- gablet_shared_api/src/kafka_events.rs
+++ gablet_shared_api/src/kafka_events.rs
@@ -1,4 +1,4 @@
 pub const CLEAR_KAFKA_FAILS: &str = "CLEAR_KAFKA_FAILS";
 pub const STOP_KAFKA_THREAD: &str = "STOP_KAFKA_THREAD";
 pub const TRACKING_TOPIC: &str = "metrics";
-pub const TRACKING_WEB_EVENT: &str = "tracking_web_event";
+pub const TRACKING_WEB_EVENT: &str = "tracking_web_events";
